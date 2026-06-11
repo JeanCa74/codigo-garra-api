a66a9ac570c1cc87a9428e5d092d3c9f4db2bba8
Jbello18 <bellojhon88@gmail.com>
2026-06-10 21:07:04 -0500

    feat: agregar endpoint GET por ID con validacion de recurso

--- internal/handlers/recurso_handler.go
+++ internal/handlers/recurso_handler.go
@@ -1,11 +1,11 @@
 package handlers
 import (
-"encoding/json"
-"net/http"
-"strconv"
-"github.com/JeanCa74/codigo-garra-api/internal/models"
-"github.com/go-chi/chi/v5"
-)
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"github.com/JeanCa74/codigo-garra-api/internal/models"
+	"github.com/go-chi/chi/v5"
+	)
 var recursosDB []models.RecursoClinico
 var recursoID int = 0
 func CreateRecurso(w http.ResponseWriter, r *http.Request) {
@@ -24,4 +24,16 @@ json.NewEncoder(w).Encode(nuevo)
 func GetRecursos(w http.ResponseWriter, r *http.Request) {
 w.Header().Set("Content-Type", "application/json")
 json.NewEncoder(w).Encode(recursosDB)
+}
+
+func GetRecurso(w http.ResponseWriter, r *http.Request) {
+id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+for _, rec := range recursosDB {
+if rec.ID == id {
+w.Header().Set("Content-Type", "application/json")
+json.NewEncoder(w).Encode(rec)
+return
+}
+}
+http.Error(w, "No encontrado", http.StatusNotFound)
 }

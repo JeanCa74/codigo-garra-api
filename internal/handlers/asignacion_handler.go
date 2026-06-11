ef9fbce5725c22b78a38ea6145869166780bfa21
Majovinces <154032367+Majovinces@users.noreply.github.com>
2026-06-10 20:01:42 -0500

    feat: implementar logica de enrutamiento y POST de asignacion

---
+++ internal/handlers/asignacion_handler.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/JeanCa74/codigo-garra-api/internal/models"
+)
+
+var asignacionesDB []models.AsignacionTriage
+var asignacionID int = 0
+
+func CreateAsignacion(w http.ResponseWriter, r *http.Request) {
+	var nueva models.AsignacionTriage
+	if err := json.NewDecoder(r.Body).Decode(&nueva); err != nil {
+		http.Error(w, "Datos inválidos", http.StatusBadRequest)
+		return
+	}
+	asignacionID++
+	nueva.ID = asignacionID
+	nueva.EstadoConfirmacion = "Pendiente"
+	asignacionesDB = append(asignacionesDB, nueva)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(nueva)
+}
